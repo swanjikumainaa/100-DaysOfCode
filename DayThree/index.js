68e6abc4582442e88074b4d6a8e8d728afe567c7
student <swanjikumaina@gmail.com>
2023-06-14 10:38:03 +0300

    Day three if else statements with js

---
+++ DayThree/index.js
@@ -0,0 +1,12 @@
+// a voting eligibility check.
+
+let age = prompt("What is your age?");
+
+
+if (age >= 18){
+    alert("You are eligible to vote!Exercise your right and make a difference.");
+
+}
+else{
+    alert("You are not eligible to vote yet.Wait until you turn 18! ");
+}
